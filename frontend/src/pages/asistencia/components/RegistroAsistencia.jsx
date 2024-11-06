12ef853346d03ebe158e0f33e201ab832fe8d604
RorberGit <rorber1978@gmail.com>
2024-11-06 07:34:39 -0500

    Optimizacion modulo asistencia 06.11.24 - Segmentacion de codigo - Optimizacion del codigo

--- frontend/src/pages/asistencia/components/RegistroAsistencia.jsx
+++ frontend/src/pages/asistencia/components/RegistroAsistencia.jsx
@@ -1,6 +1,8 @@
 import moment from "moment";
 import PropTypes from "prop-types";
 import calcularDiferenciaTiempo from "../views/calcularDiferenciaTiempo";
+import { StyledTableCell, StyledTableRow } from "./FilasCeldas";
+import calcularHorasTrabajadas from "../views/calcularHorasTrabajadas";
 
 export default function RegistroAsistencia({ registro, HoraInicio, HoraFin }) {
   //! Renderizar los datos
@@ -10,7 +12,7 @@ export default function RegistroAsistencia({ registro, HoraInicio, HoraFin }) {
 
   //! Convertir HoraEntrada y HoraSalida si existe en formato de 12 horas
   const HoraEntrada = moment(fecha_y_hora_entrada).format("h.mm A");
-  const HoraSalida = HoraSalida
+  const HoraSalida = fecha_y_hora_salida
     ? moment(fecha_y_hora_salida).format("h:mm A")
     : "Trabajando";
 
@@ -19,15 +21,63 @@ export default function RegistroAsistencia({ registro, HoraInicio, HoraFin }) {
     ? calcularDiferenciaTiempo(HoraEntrada, HoraInicio)
     : "No definido";
 
+  //! Si existe la hora de salida y a terminado de laborar calcular la diferencia de tiempo
+  const DiferenciaSalida =
+    HoraSalida !== "Trabajando"
+      ? HoraFin
+        ? calcularDiferenciaTiempo(HoraSalida, HoraFin)
+        : "No Definido"
+      : "Trabajando";
+
+  //! Si existe hora de entrada y Salida diferente de "Trabajando" calcular las horas trabajadas
+  const HorasTrabajadas =
+    HoraEntrada && HoraSalida !== "Trabajando"
+      ? calcularHorasTrabajadas(HoraEntrada, HoraSalida)
+      : "No definido";
+
+  //! Si existe una hora de entrada y la hora de inicio calcular la diferencia con la hora de entrada
+  const llegadaTarde =
+    HoraEntrada && HoraInicio
+      ? moment(HoraEntrada, "h:mm A")
+          .subtract(10, "minutes")
+          .isAfter(moment(HoraInicio, "h:mm A"))
+      : null;
+
+  //! Si Salida no es "Trabajando" y existe la hora de salida calcular la diferencia con la hora de salida
+  const salidaTemprana =
+    HoraSalida !== "Trabajando" && HoraInicio
+      ? moment(HoraSalida, "h:mm A").isBefore(moment(HoraInicio, "h:mm A"))
+      : null;
+
   return (
-    <div>
-      RegistroAsistencia
-      <div>NIP: {nip}</div>
-      <div>Nombre Completo: {NombreCompleto}</div>
-    </div>
+    <StyledTableRow>
+      <StyledTableCell>{nip}</StyledTableCell>
+      <StyledTableCell style={{ minWidth: "200px" }}>
+        {NombreCompleto}
+      </StyledTableCell>
+      <StyledTableCell style={{ minWidth: "110px" }}>
+        {HoraEntrada}
+      </StyledTableCell>
+      <StyledTableCell>
+        <span style={{ color: llegadaTarde ? "red" : "inherit" }}>
+          {DiferenciaEntrada}
+        </span>
+      </StyledTableCell>
+      <StyledTableCell style={{ minWidth: "110px" }}>
+        {HoraSalida}
+      </StyledTableCell>
+      <StyledTableCell>
+        <span style={{ color: salidaTemprana ? "red" : "inherit" }}>
+          {DiferenciaSalida}
+        </span>
+      </StyledTableCell>
+      <StyledTableCell>{HorasTrabajadas}</StyledTableCell>
+    </StyledTableRow>
   );
 }
 
 RegistroAsistencia.propTypes = {
   registro: PropTypes.object,
+  HoraInicio: PropTypes.string,
+  HoraFin: PropTypes.string,
 };
--- frontend/src/pages/asistencia/views/Registro.jsx
+++ frontend/src/pages/asistencia/views/Registro.jsx
@@ -2,112 +2,15 @@ import {
   Paper,
   Table,
   TableBody,
-  TableCell,
   TableContainer,
   TableHead,
-  TableRow,
-  styled,
-  tableCellClasses,
 } from "@mui/material";
-import moment from "moment";
-import { useCallback } from "react";
-import calcularDiferenciaTiempo from "./calcularDiferenciaTiempo";
-import calcularHorasTrabajadas from "./calcularHorasTrabajadas";
-import uuid from "../../../utilities/generarUUID";
 import PropTypes from "prop-types";
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
-    backgroundColor: theme.palette.action.hover,
-  },
-  "&:last-child td, &:last-child th": {
-    border: 0,
-  },
-}));
+import RegistroAsistencia from "../components/RegistroAsistencia";
+import generarUUID from "../../../utilities/generarUUID";
+import { StyledTableCell, StyledTableRow } from "../components/FilasCeldas";
 
 export default function Registro({ data, loading, horaEntrada, horaSalida }) {
-  const renderRow = useCallback(
-    (row) => {
-      const NIP = row.empleado.nip;
-      const NombreCompleto = `${row.empleado.nombre} ${row.empleado.apellido_paterno} ${row.empleado.apellido_materno}`;
-      const EntradaRegistrada = moment(row.fecha_y_hora_entrada)
-        .utc()
-        .format("h:mm A");
-      const DiferenciaEntrada = horaEntrada
-        ? calcularDiferenciaTiempo(EntradaRegistrada, horaEntrada)
-        : "No definido";
-
-      const SalidaRegistrada = row.fecha_y_hora_salida
-        ? moment(row.fecha_y_hora_salida).utc().format("h:mm A")
-        : "Trabajando";
-
-      const DiferenciaSalida =
-        SalidaRegistrada !== "Trabajando"
-          ? horaSalida
-            ? calcularDiferenciaTiempo(SalidaRegistrada, horaSalida)
-            : "No Definido"
-          : "Trabajando";
-
-      //! Si existe entrada registrada y SalidaRegistrada diferente de "Trabajando" calcular las horas trabajadas
-      const HorasTrabajadas =
-        EntradaRegistrada && SalidaRegistrada !== "Trabajando"
-          ? calcularHorasTrabajadas(EntradaRegistrada, SalidaRegistrada)
-          : "No definido";
-
-      //! Si existe una hora de entrada calcular la diferencia con la hora de entrada
-      const entradaTarde = EntradaRegistrada
-        ? moment(EntradaRegistrada, "h:mm A")
-            .subtract(10, "minutes")
-            .isAfter(moment(horaEntrada, "h:mm A"))
-        : null;
-
-      //! Si SalidaRegistrada no es "Trabajando" y existe la hora de salida calcular la diferencia con la hora de salida
-      const salidaTemprana =
-        SalidaRegistrada !== "Trabajando" && horaSalida
-          ? moment(SalidaRegistrada, "h:mm A").isBefore(
-              moment(horaSalida, "h:mm A")
-            )
-          : null;
-
-      return (
-        <StyledTableRow key={uuid()}>
-          <StyledTableCell>{NIP}</StyledTableCell>
-          <StyledTableCell style={{ minWidth: "200px" }}>
-            {NombreCompleto}
-          </StyledTableCell>
-          <StyledTableCell style={{ minWidth: "110px" }}>
-            {EntradaRegistrada}
-          </StyledTableCell>
-          <StyledTableCell>
-            <span style={{ color: entradaTarde ? "red" : "inherit" }}>
-              {DiferenciaEntrada}
-            </span>
-          </StyledTableCell>
-          <StyledTableCell style={{ minWidth: "110px" }}>
-            {SalidaRegistrada}
-          </StyledTableCell>
-          <StyledTableCell>
-            <span style={{ color: salidaTemprana ? "red" : "inherit" }}>
-              {DiferenciaSalida}
-            </span>
-          </StyledTableCell>
-          <StyledTableCell>{HorasTrabajadas}</StyledTableCell>
-        </StyledTableRow>
-      );
-    },
-    [horaEntrada, horaSalida]
-  );
-
   if (loading) {
     return (
       <div>
@@ -131,7 +34,17 @@ export default function Registro({ data, loading, horaEntrada, horaSalida }) {
               <StyledTableCell>Horas Trabajadas</StyledTableCell>
             </StyledTableRow>
           </TableHead>
-          <TableBody>{data && data.map(renderRow)}</TableBody>
+          <TableBody>
+            {data &&
+              data.map((registro) => (
+                <RegistroAsistencia
+                  key={generarUUID()}
+                  registro={registro}
+                  HoraInicio={horaEntrada ? horaEntrada : null}
+                  HoraFin={horaSalida ? horaSalida : null}
+                />
+              ))}
+          </TableBody>
         </Table>
       </TableContainer>
     </div>
